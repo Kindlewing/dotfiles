68e1b478b5565f0850639982fd34cad9f8c7d3bf
Hudson Finn <Hudson.Finn@gordon.edu>
2022-03-25 09:06:32 -0400

    Change plugin from vim to lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,8 +2,8 @@ local fn = vim.fn
 local Plug = fn['plug#']
 
 local function installVimPlug()
-    local command = 'curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    local command = 'sh -c \'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+                        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim\''
     os.execute(command)
 end
 
