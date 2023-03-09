7fa9e8df4185526129f65352f4a2fddd74320b13
Hudson Finn <hkfinn@tidewater.net>
2023-03-09 10:05:09 -0500

    Work on project

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,11 +1,12 @@
 # Startup Commands #
-
+neofetch
 
 # Aliases
 alias clear='clear && neofetch'
 alias zls='zellij ls'
 alias zak='zellij ka'
 alias zr='zellij run -- $argv'
+# Define an alias for the Bash script
 
 # Initialize Starship
 starship init fish | source
@@ -17,12 +18,12 @@ set -x SUDO_EDITOR /usr/bin/nvim
 set -x VISUAL /usr/bin/nvim
 set -x EDITOR /usr/bin/nvim
 set -x BROWSER /usr/bin/brave
-set ZJ_SESSIONS (zellij list-sessions)
-set NO_SESSIONS (echo $ZJ_SESSIONS | wc -l)
 
-# ZelliJ
-if test $NO_SESSIONS -ge 2
-    zellij attach (echo $ZJ_SESSIONS | sk)
-else
-    zellij attach -c
+function run_zellij_sessions
+	bash -c "~/.scripts/autostart-zellij.sh"
 end
+
+# Run the script
+run_zellij_sessions
+
+
