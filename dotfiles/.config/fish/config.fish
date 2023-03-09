680e0aa75defda7ab4d461ed7af3cf9fe11489e1
Hudson Finn <hkfinn@tidewater.net>
2023-03-08 19:09:24 -0500

    Update config

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,10 +1,24 @@
 # Startup Commands #
 neofetch
 
-# Start Zellij #
-if set -q ZELLIJ
-else
-  zellij
+# Zellij 
+
+if status is-interactive
+    # Configure auto-attach/exit to your likings (default is off).
+	set ZELLIJ_AUTO_ATTACH false
+	set ZELLIJ_AUTO_EXIT false
+	eval (zellij setup --generate-auto-start fish | string collect)
+end
+if not set -q ZELLIJ
+    if test "$ZELLIJ_AUTO_ATTACH" = "true"
+        zellij attach -c
+    else
+        zellij
+    end
+
+    if test "$ZELLIJ_AUTO_EXIT" = "true"
+        kill $fish_pid
+    end
 end
 
 alias clear='clear && neofetch'
