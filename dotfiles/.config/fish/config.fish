0184ab91a9c92af980aabe9aaace76977ed09ed8
Hudson Finn <hkfinn@tidewater.net>
2023-03-09 08:58:55 -0500

    Work on project

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,9 +1,11 @@
 # Startup Commands #
-neofetch
 
-# ZelliJ
 
+# Aliases
 alias clear='clear && neofetch'
+alias zls='zellij ls'
+alias zak='zellij ka'
+alias zr='zellij run -- $argv'
 
 # Initialize Starship
 starship init fish | source
@@ -15,3 +17,12 @@ set -x SUDO_EDITOR /usr/bin/nvim
 set -x VISUAL /usr/bin/nvim
 set -x EDITOR /usr/bin/nvim
 set -x BROWSER /usr/bin/brave
+set ZJ_SESSIONS (zellij list-sessions)
+set NO_SESSIONS (echo $ZJ_SESSIONS | wc -l)
+
+# ZelliJ
+if test $NO_SESSIONS -ge 2
+    zellij attach (echo $ZJ_SESSIONS | sk)
+else
+    zellij attach -c
+end
