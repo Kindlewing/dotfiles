b3d1f9e9358e2a859c3947d8cccc7aab4cc0ceed
Hudson Finn <hkfinn@tidewater.net>
2023-06-30 12:24:13 -0400

    Move now_playing to center

--- dotfiles/.config/polybar/launch.sh
+++ dotfiles/.config/polybar/launch.sh
@@ -7,7 +7,7 @@
   while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
 
   outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
-  tray_output=eDP-1
+  tray_output=DP-2
 
   for m in $outputs; do
     if [[ $m == "HDMI-1" ]]; then
@@ -19,7 +19,7 @@
     export MONITOR=$m
     export TRAY_POSITION=none
     if [[ $m == $tray_output ]]; then
-      TRAY_POSITION=center
+      TRAY_POSITION=right
     fi
 
     polybar --reload main </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
