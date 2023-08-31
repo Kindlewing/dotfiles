b51619dcd29f7cabcb4d2c0c093c977150d3fb8f
Hudson Finn <hudson.finn@zohomail.com>
2023-08-31 08:45:25 -0400

    Polybar

--- dotfiles/.config/polybar/launch.sh
+++ dotfiles/.config/polybar/launch.sh
@@ -18,11 +18,11 @@
   for m in $outputs; do
     export MONITOR=$m
     export TRAY_POSITION=none
-    if [[ $m == $tray_output ]]; then
+    if [[ $m == "$tray_output" ]]; then
       TRAY_POSITION=right
     fi
 
-    polybar --reload main </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+    polybar --reload main </dev/null >/var/tmp/polybar-"$m".log 2>&1 200>&- &
     disown
   done
 ) 200>/var/tmp/polybar-launch.lock
