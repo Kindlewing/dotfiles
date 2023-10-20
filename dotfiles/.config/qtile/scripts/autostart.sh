c9e53e7c93d30784bbeef39e833be970e245b5a7
hudson <hfinn@autodist.com>
2023-10-20 10:15:56 -0400

    Add background back ijn

--- dotfiles/.config/qtile/scripts/autostart.sh
+++ dotfiles/.config/qtile/scripts/autostart.sh
@@ -7,21 +7,18 @@ function run {
   fi
 }
 
-#start the conky to learn the shortcuts
-conky &
-
 #start sxhkd to replace Qtile native key-bindings
-run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc 
+run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc
 
 
 #starting utility applications at boot time
-run nm-applet
-run pamac-tray
-run xfce4-power-manage
-numlockx on
-blueberry-tray
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
-/usr/lib/xfce4/notifyd/xfce4-notifyd
+run nm-applet &
+run pamac-tray &
+run xfce4-power-manage &
+numlockx on &
+blueberry-tray &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
 
 #starting user applications at boot time
 run volumeicon &
