aba17a53ae69bd4b94f1d6cb3a034b3cfd07a5e9
Hudson Finn <hfinn@autodist.com>
2023-12-02 10:11:05 -0500

    Restructure

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,20 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	opts = {
-		defaults = {
-			file_ignore_patterns = {
-				'vendor/',
-				'node_modules/',
-				'.git',
-			},
-		},
-		pickers = {
-			find_files = {
-				hidden = true,
-				follow = true,
-				no_ignore = true,
-			},
-		},
-	},
+    'nvim-telescope/telescope.nvim',
+    opts = {
+        defaults = {
+            file_ignore_patterns = {
+                'vendor/',
+                'node_modules/',
+                'target/',
+                '.git/',
+            },
+        },
+        pickers = {
+            find_files = {
+                hidden = true,
+                follow = true,
+                no_ignore = true,
+            },
+        },
+    },
 }
