0f7a2c6b87e6e953844d4e815e41bee20c17924c
hudson <hfinn@autodist.com>
2023-12-19 15:03:19 -0500

    Move

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -1,10 +1,1 @@
-return {
-  {
-    'kylechui/nvim-surround',
-    version = '*', -- Use for stability; omit to use `main` branch for the latest features
-    event = 'VeryLazy',
-    config = function()
-      require('nvim-surround').setup {}
-    end,
-  },
-}
+/home/hudson/.dotfiles/nvim/lua/plugins/surround.lua
--- nvim/lua/register-keys.lua
+++ nvim/lua/register-keys.lua
@@ -1,8 +1,1 @@
-local wk = require('which-key')
-
-wk.register({
-	f = { name = 'file' },
-	c = { name = 'code' },
-	l = { name = 'lsp' },
-	p = { name = 'package' },
-}, { prefix = '<leader>' })
+/home/hudson/.dotfiles/nvim/lua/register-keys.lua
