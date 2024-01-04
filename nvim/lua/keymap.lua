4046807d7077254a07e9d7cc524a9a94ea1ec8e9
hudson <hfinn@autodist.com>
2024-01-04 06:19:13 -0500

    Add oil

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -49,3 +49,6 @@ map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
 map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
 map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
 map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+
+-- oil
+map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
