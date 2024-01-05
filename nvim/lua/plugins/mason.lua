6bbfaa87bf17a2f65d847617239bedfc5ed9d7c1
hudson <hfinn@autodist.com>
2024-01-05 11:27:47 -0500

    Remove eslint

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,31 +1,30 @@
 return {
-	'williamboman/mason.nvim',
-	dependencies = {
-		'williamboman/mason-lspconfig.nvim',
-	},
-	config = function()
-		require('mason').setup({
-			ui = {
-				icons = {
-					package_installed = '✓',
-					package_pending = '➜',
-					package_uninstalled = '✗',
-				},
-			},
-			ensure_installed = {
-				'stylua',
-				'eslint_d',
-				'prettier',
-			},
-		})
+    'williamboman/mason.nvim',
+    dependencies = {
+        'williamboman/mason-lspconfig.nvim',
+    },
+    config = function()
+        require('mason').setup({
+            ui = {
+                icons = {
+                    package_installed = '✓',
+                    package_pending = '➜',
+                    package_uninstalled = '✗',
+                },
+            },
+            ensure_installed = {
+                'stylua',
+                'prettier',
+            },
+        })
 
-		require('mason-lspconfig').setup({
-			ensure_installed = {
-				'lua_ls',
-				'tsserver',
-				'rust_analyzer',
-				'intelephense',
-			},
-		})
-	end,
+        require('mason-lspconfig').setup({
+            ensure_installed = {
+                'lua_ls',
+                'tsserver',
+                'rust_analyzer',
+                'intelephense',
+            },
+        })
+    end,
 }
