a4ddba744a8510a0bc283ce49d63fcd6ffbbfb12
hudson <hfinn@autodist.com>
2024-01-09 15:57:27 -0500

    Add no_ignore = true to telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,33 +1,34 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.4',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local actions = require('telescope.actions')
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.4',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local actions = require('telescope.actions')
 
-		require('telescope').setup({
-			defaults = {
-				mappings = {
-					i = {
-						['esc'] = actions.close,
-					},
-				},
-				file_ignore_patterns = {
-					'codegen.ts',
-					'.git',
-					'lazy-lock.json',
-					'node_modules',
-					'vendor',
-				},
-				dynamic_preview_title = true,
-				path_display = { 'smart' },
-			},
-			pickers = {
-				find_files = {
-					follow = true,
-					hidden = true,
-				},
-			},
-		})
-	end,
+        require('telescope').setup({
+            defaults = {
+                mappings = {
+                    i = {
+                        ['esc'] = actions.close,
+                    },
+                },
+                file_ignore_patterns = {
+                    'codegen.ts',
+                    '.git',
+                    'lazy-lock.json',
+                    'node_modules',
+                    'vendor',
+                },
+                dynamic_preview_title = true,
+                path_display = { 'smart' },
+            },
+            pickers = {
+                find_files = {
+                    follow = true,
+                    hidden = true,
+                    no_ignore = true
+                },
+            },
+        })
+    end,
 }
