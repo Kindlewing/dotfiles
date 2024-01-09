c43022d7d9cef46c84d8fa1a735366be0e87ee02
hudson <hfinn@autodist.com>
2024-01-09 06:54:37 -0500

    Prettier timeout fix

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,27 +1,27 @@
 return {
-	'stevearc/conform.nvim',
-	lazy = true,
-	event = { 'BufReadPre', 'BufNewFile' }, -- to disable, comment this out
-	config = function()
-		require('conform').setup({
-			formatters_by_ft = {
-				lua = { 'stylua' },
-				javascript = { 'prettier' },
-				typescript = { 'prettier' },
-				javascriptreact = { 'prettier' },
-				typescriptreact = { 'prettier' },
-				css = { 'prettier' },
-				html = { 'prettier' },
-				json = { 'prettier' },
-				yaml = { 'prettier' },
-				markdown = { 'prettier' },
-				php = { 'php_cs_fixer' },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				async = false,
-				lsp_fallback = true,
-			},
-		})
-	end,
+    'stevearc/conform.nvim',
+    lazy = true,
+    event = { 'BufReadPre', 'BufNewFile' }, -- to disable, comment this out
+    config = function()
+        require('conform').setup({
+            formatters_by_ft = {
+                lua = { 'stylua' },
+                javascript = { 'prettier' },
+                typescript = { 'prettier' },
+                javascriptreact = { 'prettier' },
+                typescriptreact = { 'prettier' },
+                css = { 'prettier' },
+                html = { 'prettier' },
+                json = { 'prettier' },
+                yaml = { 'prettier' },
+                markdown = { 'prettier' },
+                php = { 'php_cs_fixer' },
+            },
+            format_on_save = {
+                timeout = 1000,
+                async = false,
+                lsp_fallback = true,
+            },
+        })
+    end,
 }
