f1a105b9dd245980b0c75742e27ffb4dc8518d6f
hudson <hfinn@autodist.com>
2024-01-09 12:13:59 -0500

    Neoconf

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,31 +1,33 @@
 return {
-	'neovim/nvim-lspconfig',
-	dependencies = {
-		'folke/neodev.nvim',
-	},
-	config = function()
-		local lspconfig = require('lspconfig')
-		require('neodev').setup()
-		lspconfig.rust_analyzer.setup({})
-		lspconfig.tsserver.setup({})
-		lspconfig.intelephense.setup({})
-		lspconfig.html.setup({})
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        'folke/neodev.nvim',
+        "folke/neoconf.nvim"
+    },
+    config = function()
+        require('neodev').setup({})
+        require('neoconf').setup({})
+        local lspconfig = require('lspconfig')
+        lspconfig.rust_analyzer.setup({})
+        lspconfig.tsserver.setup({})
+        lspconfig.intelephense.setup({})
+        lspconfig.html.setup({})
 
-		lspconfig.lua_ls.setup({
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { 'vim' },
-					},
-				},
-			},
-		})
+        lspconfig.lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { 'vim' },
+                    },
+                },
+            },
+        })
 
-		-- CSS
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities.textDocument.completion.completionItem.snippetSupport =
-			true
+        -- CSS
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities.textDocument.completion.completionItem.snippetSupport =
+            true
 
-		lspconfig.cssls.setup({ capabilities = capabilities })
-	end,
+        lspconfig.cssls.setup({ capabilities = capabilities })
+    end,
 }
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,31 +1,31 @@
 return {
-	'williamboman/mason.nvim',
-	dependencies = {
-		'williamboman/mason-lspconfig.nvim',
-	},
-	config = function()
-		require('mason').setup({
-			ui = {
-				icons = {
-					package_installed = '✓',
-					package_pending = '➜',
-					package_uninstalled = '✗',
-				},
-			},
-			ensure_installed = {
-				'stylua',
-				'prettier',
-			},
-		})
+    'williamboman/mason.nvim',
+    dependencies = {
+        'williamboman/mason-lspconfig.nvim',
+    },
+    config = function()
+        require('mason').setup({
+            ui = {
+                icons = {
+                    package_installed = '✓',
+                    package_pending = '➜',
+                    package_uninstalled = '✗',
+                },
+            },
+            ensure_installed = {
+                'stylua',
+                'prettier',
+            },
+        })
 
-		require('mason-lspconfig').setup({
-			ensure_installed = {
-				'lua_ls',
-				'tsserver',
-				'cssls',
-				'rust_analyzer',
-				'intelephense',
-			},
-		})
-	end,
+        require('mason-lspconfig').setup({
+            ensure_installed = {
+                'lua_ls',
+                'tsserver',
+                'cssls',
+                'rust_analyzer',
+                'intelephense',
+            },
+        })
+    end,
 }
