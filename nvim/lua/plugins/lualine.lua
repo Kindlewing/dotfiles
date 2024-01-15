97e6371f16f837955c8e376feaf80b9c5c0775cb
hudson <hfinn@autodist.com>
2024-01-15 06:36:44 -0500

    Lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,6 +1,36 @@
 return {
 	'nvim-lualine/lualine.nvim',
 	config = function()
+		local function show_active_lsp()
+			local msg = 'No Active Lsp'
+			local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+			local clients = vim.lsp.get_active_clients()
+			if next(clients) == nil then
+				return msg
+			end
+			for _, client in ipairs(clients) do
+				local filetypes = client.config.filetypes
+				if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+					return client.name
+				end
+			end
+			return msg
+		end
+
+		local colors = {
+			bg       = '#202328',
+			fg       = '#bbc2cf',
+			yellow   = '#ECBE7B',
+			cyan     = '#008080',
+			darkblue = '#081633',
+			green    = '#98be65',
+			orange   = '#FF8800',
+			violet   = '#a9a1e1',
+			magenta  = '#c678dd',
+			blue     = '#51afef',
+			red      = '#ec5f67',
+		}
+
 		require('lualine').setup({
 			options = {
 				component_separators = '|',
@@ -20,8 +50,26 @@ return {
 					'branch',
 					{ 'diff', colored = true },
 				},
-				lualine_c = { 'diagnostics' },
-				lualine_x = {},
+				lualine_c = {
+					{
+						'diagnostics',
+						sources = { 'nvim_diagnostic' },
+						symbols = { error = ' ', warn = ' ', info = ' ' },
+						diagnostics_color = {
+							color_error = { fg = colors.red },
+							color_warn = { fg = colors.yellow },
+							color_info = { fg = colors.cyan }
+						}
+					}
+
+
+				},
+				lualine_x = {
+					{
+						show_active_lsp,
+						icon = ' LSP:',
+					}
+				},
 				lualine_y = {
 					{
 						'filetype',
