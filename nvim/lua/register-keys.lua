7f0940aebaf47a85d2bd99496078300d35417a14
hudson <hfinn@autodist.com>
2024-01-25 15:43:50 -0500

    Dap

--- nvim/lua/register-keys.lua
+++ nvim/lua/register-keys.lua
@@ -3,6 +3,7 @@ local wk = require('which-key')
 wk.register({
 	s = { name = 'search' },
 	c = { name = 'code' },
+	d = { name = 'debug' },
 	l = { name = 'lsp' },
 	g = { name = 'git' },
 	b = { name = 'buffer' },
