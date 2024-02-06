c91d4988d3359868ab12c3528e174d222724993a
hudson <hfinn@autodist.com>
2024-02-06 12:23:09 -0500

    Softt

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,7 +20,7 @@ require('lazy').setup('plugins')
 
 vim.g.gruvbox_material_transparent_background = 1
 vim.g.gruvbox_material_better_performance = 1
-vim.g.gruvbox_material_background = 'hard'
+vim.g.gruvbox_material_background = 'soft'
 vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
 
 vim.cmd.colorscheme('gruvbox-material')
