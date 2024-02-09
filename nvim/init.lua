7b907f297690a15f36bcb30b0d170773bf626c5b
hudson <hfinn@autodist.com>
2024-02-09 08:21:59 -0500

    Telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,7 +19,7 @@ require('lazy').setup('plugins')
 
 vim.g.gruvbox_material_transparent_background = 1
 vim.g.gruvbox_material_better_performance = 1
-vim.g.gruvbox_material_background = 'soft'
+vim.g.gruvbox_material_background = 'hard'
 vim.g.gruvbox_material_float_style = 'dim'
 vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
 
