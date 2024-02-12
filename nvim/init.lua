3c7bb11df88761475aec1df69a32319d865fd9a3
hudson <hfinn@autodist.com>
2024-02-12 07:36:34 -0500

    ZSH plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,9 +17,9 @@ require('icons')
 require('base')
 require('lazy').setup('plugins')
 
-vim.g.gruvbox_material_transparent_background = 1
+vim.g.gruvbox_material_transparent_background = 2
 vim.g.gruvbox_material_better_performance = 1
-vim.g.gruvbox_material_background = 'hard'
+vim.g.gruvbox_material_background = 'medium'
 vim.g.gruvbox_material_float_style = 'dim'
 vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
 
