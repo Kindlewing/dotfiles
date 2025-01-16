f38dfc5196397dcb92f1c94f7d7ae8ab0a43b4bc
Hudson Finn <hudson.finn@zohomail.com>
2025-01-16 13:50:24 -0500

    Add keybinds

---
+++ .config/wezterm/lua/keybinds.lua
@@ -0,0 +1,84 @@
+local wezterm = require("wezterm")
+
+return {
+	{
+		mods = "LEADER",
+		key = "\\",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "c",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		mods = "LEADER",
+		key = "x",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		mods = "SHIFT|ALT",
+		key = "h",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		mods = "SHIFT|ALT",
+		key = "l",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		mods = "SHIFT|ALT",
+		key = "x",
+		action = wezterm.action.CloseCurrentTab({ confirm = true }),
+	},
+	{
+		mods = "SHIFT|ALT",
+		key = "l",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		mods = "ALT",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "ALT",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "ALT",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "ALT",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		mods = "ALT",
+		key = "LeftArrow",
+		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		mods = "ALT",
+		key = "RightArrow",
+		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		mods = "ALT",
+		key = "DownArrow",
+		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		mods = "ALT",
+		key = "UpArrow",
+		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+	},
+}
