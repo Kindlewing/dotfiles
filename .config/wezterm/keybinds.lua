0ef04766f5a27b194e78b9d681c6eb9a324729a0
Hudson Finn <hudson.finn@zohomail.com>
2025-01-16 14:24:56 -0500

    Wezterm

--- .config/wezterm/keybinds.lua
+++ .config/wezterm/keybinds.lua
@@ -1,5 +1,4 @@
 local wezterm = require("wezterm")
-
 return {
 	{
 		mods = "LEADER",
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,5 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
-
 local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
@@ -13,14 +12,6 @@ config.window_decorations = "NONE"
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
 
 config.keys = require("keybinds")
-for i = 0, 9 do
-	-- leader + number to activate that tab
-	table.insert(config.keys, {
-		key = tostring(i),
-		mods = "LEADER",
-		action = wezterm.action.ActivateTab(i),
-	})
-end
 
 config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = true
