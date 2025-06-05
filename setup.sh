57b36afb20eb2192827aa6195ca3bb3e6a6de54f
Hudson Finn <hudson.finn@zohomail.com>
2025-06-05 12:42:47 -0400

    Stow

--- setup.sh
+++ setup.sh
@@ -5,9 +5,6 @@ stow_dotfiles() {
 	echo "Stowing $dir_name..."
 	stow -v .
 }
-
 sudo ./install.sh
-
-# stow the directories
 export -f stow_dotfiles
 find . -maxdepth 1 -mindepth 1 -type d ! -name ".git" ! -name ".github" ! -name "git" -exec bash -c 'stow_dotfiles "$0"' {} \;
