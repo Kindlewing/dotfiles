4d6e46bbae9e37de1640ac447ca75b9955ef8e04
Hudson Finn <hudson.finn@zohomail.com>
2025-06-26 14:54:21 -0400

    Html

--- .config/nvim/lsp/html.lua
+++ .config/nvim/lsp/html.lua
@@ -6,6 +6,6 @@ return {
 	init_options = {
 		provideFormatter = true,
 		embeddedLanguages = { css = true, javascript = true },
-		configurationSection = { "html", "css", "javascript" },
+		configurationSection = { "html", "css", "javascript", "twig" },
 	},
 }
