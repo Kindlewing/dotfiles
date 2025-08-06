ede5772be3eb9623909c8b077532ae0a2e3db467
Hudson Finn <hudson.finn@zohomail.com>
2025-08-06 15:10:45 -0400

    DAP

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -9,20 +9,16 @@ return {
 		},
 		config = function()
 			local dap = require("dap")
-			dap.adapters = {
-				php = {
-					type = "executable",
-					command = "node",
-					args = { "~/vscode-php-debug/out/phpDebug.js" },
-				},
+			dap.adapters.php = {
+				type = "executable",
+				command = "node",
+				args = { "~/vscode-php-debug/out/phpDebug.js" },
 			}
-			dap.configurations = {
-				php = {
-					type = "php",
-					request = "launch",
-					name = "Listen for Xdebug",
-					port = 9003,
-				},
+			dap.configurations.php = {
+				type = "php",
+				request = "launch",
+				name = "Listen for Xdebug",
+				port = 9003,
 			}
 		end,
 	},
