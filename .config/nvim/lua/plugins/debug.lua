c178380135917ac4157acb52b0e8f32c7c877a20
Hudson Finn <hudson.finn@zohomail.com>
2025-08-06 15:13:00 -0400

    DEBUG

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -15,10 +15,12 @@ return {
 				args = { "~/vscode-php-debug/out/phpDebug.js" },
 			}
 			dap.configurations.php = {
-				type = "php",
-				request = "launch",
-				name = "Listen for Xdebug",
-				port = 9003,
+				{
+					type = "php",
+					request = "launch",
+					name = "Listen for Xdebug",
+					port = 9003,
+				},
 			}
 		end,
 	},
