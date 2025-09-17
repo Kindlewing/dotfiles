68b80a29d3b0b821d93ed2e881b07f8fc28ca90d
Hudson Finn <hudson.finn@zohomail.com>
2025-09-17 16:32:11 -0400

    Config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,40 +1,5 @@
 return {
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			flavour = "mocha",
-			transparent_background = true,
-			styles = {
-				functions = { "italic" },
-				comments = { "italic" },
-				conditionals = { "italic" },
-			},
-			integrations = {
-				cmp = true,
-				indent_blankline = {
-					enabled = false,
-					scope_color = "sapphire",
-					colored_indent_levels = false,
-				},
-				mason = true,
-				native_lsp = { enabled = true },
-				notify = true,
-				symbols_outline = true,
-				telescope = true,
-				treesitter = true,
-				treesitter_context = true,
-			},
-			color_overrides = {
-				mocha = {
-					base = "#1e1e2e",
-				},
-			},
-		},
-	},
-	{
 		"rebelot/kanagawa.nvim",
 	},
 }
