b2a8689c5848a3766c9ee9134c5d7e80d2143e2a
Hudson Finn <hudson.finn@zohomail.com>
2026-01-17 20:59:38 -0500

    Treesitter fix

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,61 +1,58 @@
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    event = { 'BufReadPre', 'BufNewFile' },
-    build = ':TSUpdate',
-    dependencies = {
-      'windwp/nvim-ts-autotag',
-      'axelvc/template-string.nvim',
-    },
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = {
-          'tsx',
-          'lua',
-          'vim',
-          'typescript',
-          'javascript',
-          'html',
-          'css',
-          'json',
-          'graphql',
-          'regex',
-          'rust',
-          'prisma',
-          'markdown',
-          'markdown_inline',
-        },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPre", "BufNewFile" },
+		build = ":TSUpdate",
+		dependencies = {
+			"windwp/nvim-ts-autotag",
+		},
+		opts = {
+			ensure_installed = {
+				"tsx",
+				"lua",
+				"vim",
+				"typescript",
+				"javascript",
+				"html",
+				"css",
+				"json",
+				"graphql",
+				"regex",
+				"rust",
+				"prisma",
+				"markdown",
+				"markdown_inline",
+			},
 
-        sync_install = false,
+			sync_install = false,
+			auto_install = true,
 
-        auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
 
-        highlight = {
-          enable = true,
+			autotag = {
+				enable = true,
+			},
 
-          additional_vim_regex_highlighting = false,
-        },
-        autotag = {
-          enable = true,
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = '<enter>',
-            node_incremental = '<enter>',
-            scope_incremental = false,
-            node_decremental = '<bs>',
-          },
-        },
-      }
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<CR>",
+					node_incremental = "<CR>",
+					node_decremental = "<BS>",
+					scope_incremental = false,
+				},
+			},
+		},
+	},
 
-      require('template-string').setup {}
-
-      -- fold
-      local opt = vim.opt
-      opt.foldmethod = 'expr'
-      opt.foldexpr = 'nvim_treesitter#foldexpr()'
-      opt.foldenable = false
-    end,
-  },
+	{
+		"axelvc/template-string.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("template-string").setup({})
+		end,
+	},
 }
