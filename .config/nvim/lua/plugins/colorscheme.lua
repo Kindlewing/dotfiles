6d5d834e1beb3b2abafe6b11be89960a581cf21c
Hudson Finn <hudson.finn@zohomail.com>
2026-01-28 22:15:36 -0500

    colors

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -17,20 +17,7 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			-- Optionally configure and load the colorscheme
-			-- directly inside the plugin declaration.
 			vim.g.everforest_enable_italic = true
-		end,
-	},
-	{
-		"marko-cerovac/material.nvim",
-		lazy = false, -- 🔴 IMPORTANT
-		priority = 1000, -- load before others
-		config = function()
-			require("material").setup({
-				style = "darker", -- darker | lighter | oceanic | palenight | deep ocean
-			})
-			vim.cmd.colorscheme("material")
 		end,
 	},
 }
