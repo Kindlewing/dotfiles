0408541e193ba05352b12ed4a6fa217bdfccc483
Hudson Finn <hudson.finn@zohomail.com>
2026-02-09 11:08:03 -0500

    Config

--- .config/nvim/lsp/neocmake.lua
+++ .config/nvim/lsp/neocmake.lua
@@ -1,5 +1,9 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 return {
 	cmd = { "neocmakelsp", "stdio" },
 	filetypes = { "cmake" },
 	root_markers = { ".git", "build", "cmake" },
+	capabilities = capabilities,
 }
