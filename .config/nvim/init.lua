a7f6e113937e5cf0ecb93b23abf2f0801438394e
Hudson Finn <hudson.finn@zohomail.com>
2026-03-15 10:56:39 -0400

    Format

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,8 +6,12 @@ vim.g.everforest_enable_italic = true
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
 	vim.fn.system({
-		"git", "clone", "--filter=blob:none", "--branch=stable",
-		"https://github.com/folke/lazy.nvim.git", lazypath,
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
