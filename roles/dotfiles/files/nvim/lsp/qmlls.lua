18e40120e5d00f1ebefadbeff7626c1924ca4b98
Hudson Finn <hudson.finn@zohomail.com>
2026-04-30 17:15:56 -0400

    Quickshell

--- roles/dotfiles/files/nvim/lsp/qmlls.lua
+++ roles/dotfiles/files/nvim/lsp/qmlls.lua
@@ -1,6 +1,6 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { 'qmlls', '-I', '/usr/lib/qt6/qml' },
-  filetypes = { 'qml', 'qmljs' },
-  root_markers = { '.git' },
+	cmd = { "qmlls", "-I", "/usr/lib/qt6/qml" },
+	filetypes = { "qml", "qmljs" },
+	root_markers = { ".qmlls.ini", ".git" },
 }
